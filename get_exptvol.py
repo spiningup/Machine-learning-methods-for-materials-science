3c8463353a20cdf784ee6f534b620df4879673d4
spiningup <mouseyan@gmail.com>
2014-05-08 11:30:05 -0600

    update exptvol

--- get_exptvol.py
+++ get_exptvol.py
@@ -18,15 +18,16 @@ for item in d:
     icsdstr = "{0:06d}".format(int(item["icsdnum"]))
     distinctnames = Counter(names)
 
-    # copy cif files
-#    if not os.path.isfile("/home/jyan/icsd/%s/icsd_%s.cif"%(name, icsdstr)):
-#        for name in distinctnames.keys():
-#            if os.path.isfile("/home/jyan/icsd/%s/icsd_%s.cif"%(name, icsdstr)):
-#                break
-        
-#    print name, icsdstr
-#    atoms = read.icsd_cif_a("/home/jyan/icsd/%s/icsd_%s.cif"%(name, icsdstr))        
-#    shutil.copy2("/home/jyan/icsd/%s/icsd_%s.cif"%(name, icsdstr), 'structures/icsd_%s.cif'%(icsdstr))
+    if not os.path.isfile("structures/icsd_%s.cif"%(icsdstr)):
+        # copy cif files
+        if not os.path.isfile("/home/jyan/icsd/%s/icsd_%s.cif"%(name, icsdstr)):
+            for name in distinctnames.keys():
+                if os.path.isfile("/home/jyan/icsd/%s/icsd_%s.cif"%(name, icsdstr)):
+                    break
+            
+    #    print name, icsdstr
+        atoms = read.icsd_cif_a("/home/jyan/icsd/%s/icsd_%s.cif"%(name, icsdstr))        
+        shutil.copy2("/home/jyan/icsd/%s/icsd_%s.cif"%(name, icsdstr), 'structures/icsd_%s.cif'%(icsdstr))
 
     # get volume per atom
     atoms = read.icsd_cif_a("structures/icsd_%s.cif"%(icsdstr)) 
