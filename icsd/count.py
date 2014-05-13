6a050354033866257114b8ecde6c2b11deefc5c0
spiningup <mouseyan@gmail.com>
2014-05-13 17:09:22 -0600

    count total number of files

--- icsd/count.py
+++ icsd/count.py
@@ -2,6 +2,10 @@ import numpy as np
 import os
 import commands
 
+count = 0
 dirs = os.listdir('/home/jyan/icsd')
 for dir in dirs:
-    print dir, commands.getoutput("ls /home/jyan/icsd/%s/ | wc -l"%(dir))
+    number = commands.getoutput("ls /home/jyan/icsd/%s/ | wc -l"%(dir))
+    print dir, number
+    count += int(number)
+print count
