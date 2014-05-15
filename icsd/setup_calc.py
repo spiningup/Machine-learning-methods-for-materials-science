77de9c39f22c5304e45dac45e4ad99feb664bc6b
spiningup <mouseyan@gmail.com>
2014-05-15 13:02:01 -0600

    be able to remove files

--- icsd/setup_calc.py
+++ icsd/setup_calc.py
@@ -1,11 +1,13 @@
 import numpy as np
 from filterdb import *
 import shutil
+import os
+import commands
 
 d = json.load(open('icsd.json', 'r'))
 dnew = formula_with_multiple_spacegroups(d)    
 d = filter_natoms(d, nmax=10)
-for el in (['D', 'O', 'H']):
+for el in (['D', 'O', 'H', 'Re', 'Ta', 'Cs', 'Tl', 'Os', 'Tc']):
     d = filter_elements(d, el, incl=False)
 print "number of atoms < 10", len(d)
 d = add_otherspacegroups(d, dnew)
@@ -29,5 +31,16 @@ for key, item in d.items():
         if os.path.isfile("/home/jyan/icsd/%s/icsd_%s.cif"%(el, key)):
             break
     filename = "/home/jyan/icsd/%s/icsd_%s.cif"%(el, key)
-    shutil.copy2(filename, "/scratch/jyan/ML_natoms_10/icsd_%s.cif"%(key))
-    
+    filename2 = "/scratch/jyan/ML_natoms_10/icsd_%s.cif"%(key)
+
+    if not os.path.isfile(filename2):
+        shutil.copy2(filename, filename2)
+
+allcifs =  commands.getoutput("ls /scratch/jyan/ML_natoms_10/*.cif").split('\n')
+for cif in allcifs:
+    if cif[-10:-4] not in d.keys():
+        os.remove(cif) 
+        if os.path.isfile(cif[:-4]):
+            shutil.rmtree(cif[:-4])
+
+
