1d9c758d9661b1b3949347f6ca071a1d152a92e3
spiningup <mouseyan@gmail.com>
2014-05-21 13:24:09 -0600

    update extractenergy

--- icsd/extractenergy.py
+++ icsd/extractenergy.py
@@ -3,6 +3,7 @@ import os
 import json
 import numpy as np
 from pylada import vasp
+from pylada.crystal import read
 
 jobdir = '/scratch/jyan/ML_natoms_10'
 
@@ -22,20 +23,23 @@ for cif in allcifs:
             E.append(a.total_energy.magnitude)
             dirs.append(subdir)
 #            print "%6s/%20s"%(icsdno,subdir), a.total_energy
-    print icsdno, len(E)
+#    print icsdno, len(E)
 #    if len(E) > 0: print icsdno, min(E)
-    if 0:#len(E) > 1:
+    if len(E) > 1:
         minidx = np.argsort(E)[0]
         minE = min(E)
         minEdir = dirs[minidx]
         E.pop(minidx)
         dirs.pop(minidx)
         if minEdir == "non-magnetic":
-            if np.mean(E) - minE < 1: # print the ones with energy difference smaller than 1 eV
-                print "%s/%20s,%6.2f, %3d, %6.2f, %6.2f"%(icsdno, minEdir, minE, len(E)+1, np.mean(E), np.std(E))
+            if np.mean(E) - minE < 1 and np.std(E) > 0.05: # print the ones with energy difference smaller than 1 eV
+                atoms = read.poscar('%s/%s/%s/CONTCAR'%(jobdir, icsdno, "hs-ferro/relax_cellshape/0"))
+                print "%s/%20s,%6.2f, %3d, %6.2f, %6.2f"%(icsdno, minEdir, minE, len(E)+1, np.mean(E), np.std(E)/len(atoms))
         else:
             nonmagidx = [j for j in range(len(dirs)) if dirs[j] == "non-magnetic"][0]
             nonmagE = E[nonmagidx]
             E.pop(nonmagidx)
-            print "%s/%20s,%6.2f, %3d, non-magnetic, %6.2f, %6.2f, %6.2f"%(icsdno, minEdir, minE, len(E)+2, nonmagE, np.mean(E), np.std(E))
+            if np.std(E) > 0.05:
+                atoms = read.poscar('%s/%s/%s/CONTCAR'%(jobdir, icsdno, "hs-ferro/relax_cellshape/0"))
+                print "%s/%20s,%6.2f, %3d, non-magnetic, %6.2f, %6.2f, %6.2f"%(icsdno, minEdir, minE, len(E)+2, nonmagE, np.mean(E), np.std(E)/len(atoms))
 
