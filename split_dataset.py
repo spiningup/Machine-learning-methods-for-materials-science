11a106aeddc325481538949649087ce218bf869a
spiningup <mouseyan@gmail.com>
2014-05-21 13:26:22 -0600

    more split dataset

--- split_dataset.py
+++ split_dataset.py
@@ -26,3 +26,15 @@ def get_train_validation_set(mset):
         
     return mtrain, mcross, mset # mset is a sum of mtrain and mcross
     
+def get_partial_dataset(mset, choice=1):
+    if choice == 5: return mset
+    mset = mset[:len(mset)//5*5] 
+    mnewset = []
+    for j in range(choice):
+        i = 0
+        while i < len(mset):
+            idx = np.random.randint(0, 5-j)
+            mnewset.append(mset[i+idx])
+            mset.pop(i+idx)
+            i += 5 - j - 1
+    return mnewset
