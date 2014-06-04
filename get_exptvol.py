e2fb68214b9bc3e59439d2a5393709d24c63e152
spiningup <mouseyan@gmail.com>
2014-06-04 11:08:42 -0600

    update structures

--- get_exptvol.py
+++ get_exptvol.py
@@ -18,26 +18,15 @@ for item in d:
     icsdstr = "{0:06d}".format(int(item["icsdnum"]))
     distinctnames = Counter(names)
 
-    if not os.path.isfile("structures/icsd_%s.cif"%(icsdstr)):
-        # copy cif files
-        if not os.path.isfile("/home/jyan/icsd/%s/icsd_%s.cif"%(name, icsdstr)):
-            for name in distinctnames.keys():
-                if os.path.isfile("/home/jyan/icsd/%s/icsd_%s.cif"%(name, icsdstr)):
-                    break
-            
-    #    print name, icsdstr
-        atoms = read.icsd_cif_a("/home/jyan/icsd/%s/icsd_%s.cif"%(name, icsdstr))        
-        shutil.copy2("/home/jyan/icsd/%s/icsd_%s.cif"%(name, icsdstr), 'structures/icsd_%s.cif'%(icsdstr))
-
     # get volume per atom
-    atoms = read.icsd_cif_a("structures/icsd_%s.cif"%(icsdstr)) 
+    atoms = read.icsd_cif_a("/scratch/jyan/allcifs/icsd_%s.cif"%(icsdstr)) 
     vol = atoms.volume.magnitude / len(atoms)
 
     # get a,b,c, alpha,beta,gamma
     values = np.zeros(7)
     for i, name in enumerate(["_cell_length_a", "_cell_length_b", "_cell_length_c", 
                              "_cell_angle_alpha", "_cell_angle_beta", "_cell_angle_gamma", "_cell_volume"]):
-        values[i] = float(commands.getoutput("grep %s structures/icsd_%s.cif"%(name, icsdstr)).split()[1].split("(")[0])
+        values[i] = float(commands.getoutput("grep %s /scratch/jyan/allcifs/icsd_%s.cif"%(name, icsdstr)).split()[1].split("(")[0])
 
     values[0:3] /= (values[6] / vol)**(1./3.)
     values[6] = vol
